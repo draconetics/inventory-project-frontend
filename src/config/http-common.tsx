2ad2f853377b8a1bb5afbd4238b84d0d03d3f73b
dracon <marioflorescondori@gmail.com>
2021-09-05 07:45:21 -0400

    adding external backend url

--- src/config/http-common.tsx
+++ src/config/http-common.tsx
@@ -1,7 +1,8 @@
 import axios,  { AxiosInstance } from "axios";
 
 export const http:AxiosInstance =  axios.create({
-  baseURL: "http://localhost:4000",
+  //baseURL: "http://localhost:4000",
+  baseURL: "https://inventory-project-backend.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
